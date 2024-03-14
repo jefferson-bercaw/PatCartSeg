b407e631110bd10776e0fa8516f4ff9a8f1174ca
jrb187 <jefferson.bercaw@duke.edu>
2024-03-14 12:44:13 -0400

    Added double_conv_block

--- unet.py
+++ unet.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras imort layers
+from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
+
+## Model Creation
+def double_conv_block(lyr, n_filt, kernel_size):
+    lyr = layers.Conv2D(n_filt, kernel_size, padding="same", activation="relu", kernel_initializer="he_normal")(lyr)
+    lyr = layers.Conv2D(n_filt, kernel_size, padding="same", activation="relu", kernel_initializer="he_normal")(lyr)
+    return lyr
+
 
