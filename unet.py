6201593135422b8a28e3ff77dd51a8e9e7b8bc62
jrb187 <jefferson.bercaw@duke.edu>
2024-03-14 12:46:10 -0400

    Added downsample_block

--- unet.py
+++ unet.py
@@ -8,6 +8,15 @@ import matplotlib.pyplot as plt
 def double_conv_block(lyr, n_filt, kernel_size):
     lyr = layers.Conv2D(n_filt, kernel_size, padding="same", activation="relu", kernel_initializer="he_normal")(lyr)
     lyr = layers.Conv2D(n_filt, kernel_size, padding="same", activation="relu", kernel_initializer="he_normal")(lyr)
+
     return lyr
+
+
+def downsample_block(lyr, n_filt, kernel_size):
+    f = double_conv_block(lyr, n_filt, kernel_size)
+    p = layers.MaxPool2D(2)(f)
+    p = layers.Dropout(0.3)(p)
+
+    return f, p
 
 
