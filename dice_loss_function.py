8919f9d8eb8c8179cf1be4d3f0cf27f9e8078451
jrb187 <jefferson.bercaw@duke.edu>
2024-04-01 10:42:59 -0400

    PEP8

--- dice_loss_function.py
+++ dice_loss_function.py
@@ -7,5 +7,6 @@ def dice_coefficient(y_true, y_pred):
     union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred)
     return (2. * intersection + smooth) / (union + smooth)
 
+
 def dice_loss(y_true, y_pred):
     return 1 - dice_coefficient(y_true, y_pred)
