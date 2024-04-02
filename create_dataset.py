98effd2a03090caad86c7eff3b584f3954484067
jrb187 <jefferson.bercaw@duke.edu>
2024-04-02 17:14:12 -0400

    Attempt to use .npz files. Will not work. Will need to use separate files for P, PC, and background so we can save a in .bmp format

--- create_dataset.py
+++ create_dataset.py
@@ -1,69 +1,43 @@
 import tensorflow as tf
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import numpy as np
 
 
-def load_image_and_label(image_file, label):
-    # Load and preprocess the image
-    image = tf.io.read_file(image_file)
-    image = tf.image.decode_image(image, channels=1)  # Adjust channels as per your data
-    # Perform any additional preprocessing as needed
+def load_data(image_path, mask_path):
 
-    # Convert label to tensor (assuming it's already in the correct format)
-    label = tf.convert_to_tensor(label)
+    # image_byte = image_path.numpy()
+    # mask_byte = mask_path.numpy()
 
-    return image, label
+    image = tf.io.read_file(image_path)
+    mask = tf.io.read_file(mask_path)
 
+    image = tf.io.decode_image(image)
+    mask = tf.io.decode_image(mask)
 
-def process_paths(two_paths):
-    return tf.io.read_file(two_paths[0]), tf.io.read_file(two_paths[1])
+    # image = np.load(image_str)
+    # image = image['arr']
+    # image = image.astype(np.float64) / 255.0
+    #
+    # mask = np.load(mask_str)
+    # mask = mask['arr']
 
-
-def parse_image(filename):
-    # Load the npz file
-    image_npz = np.load(filename)
-    # Extract the image array from the npz file
-    image = image_npz['image']
-    # Normalize the pixel values to [0, 1]
-    image = image.astype(np.float32) / 255.0
-    return image
+    return image, mask
 
 
-def parse_mask(filename):
-    # Load the npz file
-    mask_npz = np.load(filename)
-    # Extract the mask array from the npz file
-    mask = mask_npz['mask']
-    # Normalize the pixel values to [0, 1]
-    mask = mask.astype(np.float32) / 255.0
-    return mask
-
-
-def create_dataset(image_files, mask_files, batch_size=32, shuffle=True):
+def create_dataset(image_dir, mask_dir):
     # Create dataset from list of image files
-    image_dataset = tf.data.Dataset.from_tensor_slices(image_files)
-    # Map the parse_image function to decode and preprocess images
-    image_dataset = image_dataset.map(lambda x: tf.numpy_function(parse_image, [x], tf.float32), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    mri = tf.data.Dataset.list_files(image_dir, shuffle=False)
+    mask = tf.data.Dataset.list_files(mask_dir, shuffle=False)
 
-    # Create dataset from list of mask files
-    mask_dataset = tf.data.Dataset.from_tensor_slices(mask_files)
-    # Map the parse_mask function to decode and preprocess masks
-    mask_dataset = mask_dataset.map(lambda x: tf.numpy_function(parse_mask, [x], tf.float32), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = tf.data.Dataset.zip((mri, mask))
 
-    # Combine the image and mask datasets
-    dataset = tf.data.Dataset.zip((image_dataset, mask_dataset))
-
-    # Shuffle and batch the dataset
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=len(image_files))
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.map(load_data)
 
     return dataset
 
 
 def get_dataset(batch_size, dataset_type):
-
     if dataset_type == 'train':
         images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mri"
         masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mask_3d"
@@ -74,14 +48,15 @@ def get_dataset(batch_size, dataset_type):
         images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/val/mri"
         masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/val/mask_3d"
     else:
-        raise(ValueError, "The value {dataset_type} for the variable dataset_type is not one of0 'train', 'test', or 'val'")
+        raise(ValueError, f"The value {dataset_type} for the variable dataset_type is not one of 'train', 'test', "
+                          f"or 'val'")
 
-    # Create a dataset of file paths
-    image_files = [os.path.join(images_dir, file) for file in os.listdir(images_dir) if file.endswith('.npz')]
-    label_files = [os.path.join(masks_dir, file) for file in os.listdir(masks_dir) if file.endswith('.npz')]
+    # Add .npz extension
+    images_dir = images_dir + "/*.npz"
+    masks_dir = masks_dir + "/*.npz"
 
     # Create dataset
-    dataset = create_dataset(image_files, label_files, batch_size=batch_size, shuffle=True)
+    dataset = create_dataset(images_dir, masks_dir)
 
     return dataset
 
@@ -89,11 +64,15 @@ def get_dataset(batch_size, dataset_type):
 if __name__ == '__main__':
     # Hyperparameters
     batch_size = 32
-    images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mri"
-    masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mask_3d"
+    images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mri/*.npz"
+    masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mask_3d/*.npz"
 
     # Create a dataset of file paths
-    image_files = [os.path.join(images_dir, file) for file in os.listdir(images_dir) if file.endswith('.npz')]
-    label_files = [os.path.join(masks_dir, file) for file in os.listdir(masks_dir) if file.endswith('.npz')]
+    # image_files = [os.path.join(images_dir, file) for file in os.listdir(images_dir) if file.endswith('.npz')]
+    # label_files = [os.path.join(masks_dir, file) for file in os.listdir(masks_dir) if file.endswith('.npz')]
 
-    dataset = create_dataset(image_files, label_files, batch_size=32, shuffle=True)
+    dataset = create_dataset(images_dir, masks_dir)
+
+    for mri, mask in dataset:
+        print(mri, mask)
+        break
