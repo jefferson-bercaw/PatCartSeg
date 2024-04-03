dd9e9b74356974f66e5d3cd6c9c8ec449e01e9d7
jrb187 <jefferson.bercaw@duke.edu>
2024-04-03 09:10:49 -0400

    Added get_data_path function to return the absolute directory of the data folder we're working on, whether on the HPC or on a local machine

---
+++ get_data_path.py
@@ -0,0 +1,11 @@
+import os
+
+
+def get_data_path():
+    cur_dir = os.getcwd()
+    if cur_dir[0:2] == "R:" | cur_dir[0:2] = "C:":  # If on local machine
+        data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_BMP2"
+    else:  # If on cluster
+        data_path = "/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_BMP2"
+
+    return data_path
