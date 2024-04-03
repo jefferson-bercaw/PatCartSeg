33c92d21ebfec5c5e855be963f6ae69e070bcbea
jrb187 <jefferson.bercaw@duke.edu>
2024-04-03 08:57:24 -0400

    Changed to handle a 2d mask input containing data about each tissue in the bit depth

--- create_dataset.py
+++ create_dataset.py
@@ -4,25 +4,50 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import numpy as np
 
 
-def load_data(image_path, mask_P_path, mask_PC_path):
+def assemble_3d_mask(mask_2d, xy_dim):
+    mask = np.zeros((xy_dim, xy_dim, 3))
+
+    bckgrnd = np.zeros((xy_dim, xy_dim))
+    p = np.zeros((xy_dim, xy_dim))
+    pc = np.zeros((xy_dim, xy_dim))
+
+    background_inds = mask_2d == 0
+    bckgrnd[background_inds] = 1
+
+    p_mask_inds = mask_2d == 1
+    p[p_mask_inds] = 1
+
+    pc_mask_inds = mask_2d == 2
+    pc[pc_mask_inds] = 1
+
+    mask[:, :, 0] = bckgrnd
+    mask[:, :, 1] = p
+    mask[:, :, 2] = pc
+
+    return mask
+
+
+def load_data(image_path, mask_path):
     image = tf.io.read_file(image_path)
-    mask_P = tf.io.read_file(mask_P_path)
-    mask_PC = tf.io.read_file(mask_PC_path)
+    mask = tf.io.read_file(mask_path)
 
-    image = image.astype(np.float64) / 255.0
-    mask_P = mask_P.astype(np.float64)
-    mask_PC = mask_PC.astype(np.float64)
+    image = tf.image.decode_bmp(image)
+    mask = tf.image.decode_bmp(mask)
 
-    return image, mask_P, mask_PC
+    mask_3d = assemble_3d_mask(mask, 512)
+
+    image = tf.cast(image, tf.float64) / 255.0
+    mask_3d = tf.cast(mask_3d, tf.float64)
+
+    return image, mask_3d
 
 
-def create_dataset(image_dir, mask_P_dir, mask_PC_dir):
+def create_dataset(image_dir, mask_dir):
     # Create dataset from list of image files
     mri = tf.data.Dataset.list_files(image_dir, shuffle=False)
-    mask_P = tf.data.Dataset.list_files(mask_P_dir, shuffle=False)
-    mask_PC = tf.data.Dataset.list_files(mask_PC_dir, shuffle=False)
+    mask = tf.data.Dataset.list_files(mask_dir, shuffle=False)
 
-    dataset = tf.data.Dataset.zip((mri, mask_P, mask_PC))
+    dataset = tf.data.Dataset.zip((mri, mask))
 
     dataset = dataset.map(load_data)
 
@@ -31,28 +56,24 @@ def create_dataset(image_dir, mask_P_dir, mask_PC_dir):
 
 def get_dataset(batch_size, dataset_type):
     if dataset_type == 'train':
-        images_dir = "../Split_Data_BMP/train/mri"
-        mask_P_dir = "../Split_Data_BMP/train/mask_P"
-        mask_PC_dir = "../Split_Data_BMP/train/mask_PC"
+        images_dir = "../Split_Data_BMP2/train/mri"
+        mask_dir = "../Split_Data_BMP2/train/mask"
     elif dataset_type == 'test':
-        images_dir = "../Split_Data_BMP/test/mri"
-        mask_P_dir = "../Split_Data_BMP/test/mask_P"
-        mask_PC_dir = "../Split_Data_BMP/test/mask_PC"
+        images_dir = "../Split_Data_BMP2/test/mri"
+        mask_dir = "../Split_Data_BMP2/test/mask"
     elif dataset_type == 'val' or dataset_type == 'validation':
-        images_dir = "../Split_Data_BMP/val/mri"
-        mask_P_dir = "../Split_Data_BMP/val/mask_P"
-        mask_PC_dir = "../Split_Data_BMP/val/mask_PC"
+        images_dir = "../Split_Data_BMP2/val/mri"
+        mask_dir = "../Split_Data_BMP2/val/mask"
     else:
         raise(ValueError, f"The value {dataset_type} for the variable dataset_type is not one of 'train', 'test', "
                           f"or 'val'")
 
     # Add bmp wildcard
     images_dir = images_dir + "/*.bmp"
-    mask_P_dir = mask_P_dir + "/*.bmp"
-    mask_PC_dir = mask_PC_dir + "/*.bmp"
+    mask_dir = mask_dir + "/*.bmp"
 
     # Create dataset
-    dataset = create_dataset(images_dir, mask_P_dir, mask_PC_dir)
+    dataset = create_dataset(images_dir, mask_dir)
 
     return dataset
 
@@ -60,13 +81,7 @@ def get_dataset(batch_size, dataset_type):
 if __name__ == '__main__':
     # Hyperparameters
     batch_size = 32
-    dataset = get_dataset(batch_size=batch_size, 'train')
-
-    # Create a dataset of file paths
-    # image_files = [os.path.join(images_dir, file) for file in os.listdir(images_dir) if file.endswith('.npz')]
-    # label_files = [os.path.join(masks_dir, file) for file in os.listdir(masks_dir) if file.endswith('.npz')]
-
-    dataset = create_dataset(images_dir, masks_dir)
+    dataset = get_dataset(batch_size=batch_size, dataset_type='train')
 
     for mri, mask in dataset:
         print(mri, mask)
