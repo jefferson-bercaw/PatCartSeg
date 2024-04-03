664a6ee35b27ec17449a58f34b63808a6572ff74
jrb187 <jefferson.bercaw@duke.edu>
2024-04-03 08:25:01 -0400

    Rearranged to load in two separate masks from two separate locations.

--- create_dataset.py
+++ create_dataset.py
@@ -4,33 +4,25 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import numpy as np
 
 
-def load_data(image_path, mask_path):
-
-    # image_byte = image_path.numpy()
-    # mask_byte = mask_path.numpy()
-
+def load_data(image_path, mask_P_path, mask_PC_path):
     image = tf.io.read_file(image_path)
-    mask = tf.io.read_file(mask_path)
+    mask_P = tf.io.read_file(mask_P_path)
+    mask_PC = tf.io.read_file(mask_PC_path)
 
-    image = tf.io.decode_image(image)
-    mask = tf.io.decode_image(mask)
+    image = image.astype(np.float64) / 255.0
+    mask_P = mask_P.astype(np.float64)
+    mask_PC = mask_PC.astype(np.float64)
 
-    # image = np.load(image_str)
-    # image = image['arr']
-    # image = image.astype(np.float64) / 255.0
-    #
-    # mask = np.load(mask_str)
-    # mask = mask['arr']
-
-    return image, mask
+    return image, mask_P, mask_PC
 
 
-def create_dataset(image_dir, mask_dir):
+def create_dataset(image_dir, mask_P_dir, mask_PC_dir):
     # Create dataset from list of image files
     mri = tf.data.Dataset.list_files(image_dir, shuffle=False)
-    mask = tf.data.Dataset.list_files(mask_dir, shuffle=False)
+    mask_P = tf.data.Dataset.list_files(mask_P_dir, shuffle=False)
+    mask_PC = tf.data.Dataset.list_files(mask_PC_dir, shuffle=False)
 
-    dataset = tf.data.Dataset.zip((mri, mask))
+    dataset = tf.data.Dataset.zip((mri, mask_P, mask_PC))
 
     dataset = dataset.map(load_data)
 
@@ -39,24 +31,28 @@ def create_dataset(image_dir, mask_dir):
 
 def get_dataset(batch_size, dataset_type):
     if dataset_type == 'train':
-        images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mri"
-        masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mask_3d"
+        images_dir = "../Split_Data_BMP/train/mri"
+        mask_P_dir = "../Split_Data_BMP/train/mask_P"
+        mask_PC_dir = "../Split_Data_BMP/train/mask_PC"
     elif dataset_type == 'test':
-        images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/test/mri"
-        masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/test/mask_3d"
+        images_dir = "../Split_Data_BMP/test/mri"
+        mask_P_dir = "../Split_Data_BMP/test/mask_P"
+        mask_PC_dir = "../Split_Data_BMP/test/mask_PC"
     elif dataset_type == 'val' or dataset_type == 'validation':
-        images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/val/mri"
-        masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/val/mask_3d"
+        images_dir = "../Split_Data_BMP/val/mri"
+        mask_P_dir = "../Split_Data_BMP/val/mask_P"
+        mask_PC_dir = "../Split_Data_BMP/val/mask_PC"
     else:
         raise(ValueError, f"The value {dataset_type} for the variable dataset_type is not one of 'train', 'test', "
                           f"or 'val'")
 
-    # Add .npz extension
-    images_dir = images_dir + "/*.npz"
-    masks_dir = masks_dir + "/*.npz"
+    # Add bmp wildcard
+    images_dir = images_dir + "/*.bmp"
+    mask_P_dir = mask_P_dir + "/*.bmp"
+    mask_PC_dir = mask_PC_dir + "/*.bmp"
 
     # Create dataset
-    dataset = create_dataset(images_dir, masks_dir)
+    dataset = create_dataset(images_dir, mask_P_dir, mask_PC_dir)
 
     return dataset
 
@@ -64,8 +60,7 @@ def get_dataset(batch_size, dataset_type):
 if __name__ == '__main__':
     # Hyperparameters
     batch_size = 32
-    images_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mri/*.npz"
-    masks_dir = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data/train/mask_3d/*.npz"
+    dataset = get_dataset(batch_size=batch_size, 'train')
 
     # Create a dataset of file paths
     # image_files = [os.path.join(images_dir, file) for file in os.listdir(images_dir) if file.endswith('.npz')]
