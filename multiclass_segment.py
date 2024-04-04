bf5859860e1cd53f1d9a60fa06aecd467f2a8a5d
jrb187 <jefferson.bercaw@duke.edu>
2024-04-04 09:49:19 -0400

    Save model bug fix, saving history as .pkl file, and changed epochs from 10 to 5

--- multiclass_segment.py
+++ multiclass_segment.py
@@ -1,7 +1,9 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import datetime
 import tensorflow as tf
 import numpy as np
+import pickle
 
 from unet import build_unet
 from dice_loss_function import dice_loss
@@ -11,7 +13,7 @@ if __name__ == "__main__":
     # Hyperparameters
     batch_size = 16
     dropout_rate = 0.3
-    epochs = 10
+    epochs = 5
 
     # Build and compile model
     unet_model = build_unet(dropout_rate=dropout_rate)
@@ -23,5 +25,14 @@ if __name__ == "__main__":
     test_dataset = get_dataset(batch_size=batch_size, dataset_type='test')
 
     # Train model
-    unet_model.fit(train_dataset, epochs=epochs)
-    unet_model.save("Unet_Model")
+    history = unet_model.fit(train_dataset, epochs=epochs)
+
+    # Save model
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_name = f"unet_{current_time}.h5"
+    unet_model.save(f"./models/{model_name}")
+
+    # Save history
+    hist_name = f"unet_{current_time}.pkl"
+    with open(f"./history/{hist_name}", "wb") as f:
+        pickle.dump(history.history, f)
