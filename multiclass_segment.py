2050acd9e51d9efa8f148aa08fb62eea68e98786
jrb187 <jefferson.bercaw@duke.edu>
2024-04-05 15:05:34 -0400

    Changed loss to binary crossentropy

--- multiclass_segment.py
+++ multiclass_segment.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     # Build and compile model
     unet_model = build_unet(dropout_rate=dropout_rate)
     unet_model.compile(optimizer='adam',
-                       loss=dice_loss,
+                       loss=tf.keras.losses.BinaryCrossentropy,
                        metrics=['accuracy',
                                 tf.keras.metrics.FalsePositives(thresholds=0.5, name='FP'),
                                 tf.keras.metrics.FalseNegatives(thresholds=0.5, name='FN'),
