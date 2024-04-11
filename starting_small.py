5a597328057b9ef0c68c6277144abd0340156831
jrb187 <jefferson.bercaw@duke.edu>
2024-04-11 14:39:04 -0400

    Added central storage strategy and printing if GPUs are available.

--- starting_small.py
+++ starting_small.py
@@ -30,45 +30,55 @@ def convert_mask(mask_tensor):
 
 
 if __name__ == '__main__':
+    # See if GPU is being used
+    print("TensorFlow version:", tf.__version__)
 
-    start_path = get_data_path()
+    if tf.config.list_physical_devices("GPU"):
+        print("GPU is Available")
+    else:
+        print("No GPU detected")
 
-    mri_path = start_path + "/train/mri"
-    label_path = start_path + "/train/mask"
+    central_storage_strategy = tf.distribute.experimental.CentralStorageStrategy()
 
-    mri = os.listdir(mri_path)
-    mask = os.listdir(label_path)
+    with central_storage_strategy.scope():
+        start_path = get_data_path()
 
-    mri.sort()
-    mask.sort()
+        mri_path = start_path + "/train/mri"
+        label_path = start_path + "/train/mask"
 
-    # Train/test split
-    X_train_files = mri[:720]
-    y_train_files = mask[:720]
+        mri = os.listdir(mri_path)
+        mask = os.listdir(label_path)
 
-    X_test_files = mri[720:840]
-    y_test_files = mask[720:840]
+        mri.sort()
+        mask.sort()
 
-    X_train = assemble_tensor(X_train_files, mri_path)
-    y_train = convert_mask(assemble_tensor(y_train_files, label_path))
-    X_test = assemble_tensor(X_test_files, mri_path)
-    y_test = convert_mask(assemble_tensor(y_test_files, label_path))
+        # Train/test split
+        X_train_files = mri[:720]
+        y_train_files = mask[:720]
 
-    # Training
-    batch_size = 32
-    dropout_rate = 0.3
-    epochs = 500
-    patience = 10
-    min_delta = 0.001
+        X_test_files = mri[720:840]
+        y_test_files = mask[720:840]
 
-    unet_model = build_unet(dropout_rate=dropout_rate)
-    unet_model.compile(optimizer='adam',
-                       loss=dice_loss,
-                       metrics=['accuracy',
-                                tf.keras.metrics.FalsePositives(thresholds=0.5, name='FP'),
-                                tf.keras.metrics.FalseNegatives(thresholds=0.5, name='FN'),
-                                tf.keras.metrics.TruePositives(thresholds=0.5, name='TP'),
-                                tf.keras.metrics.TrueNegatives(thresholds=0.5, name='TN')])
-    history = unet_model.fit(X_train, y_train,
-                             epochs=epochs)
-    unet_model.save(filename="Small_Unet.keras")
+        X_train = assemble_tensor(X_train_files, mri_path)
+        y_train = convert_mask(assemble_tensor(y_train_files, label_path))
+        X_test = assemble_tensor(X_test_files, mri_path)
+        y_test = convert_mask(assemble_tensor(y_test_files, label_path))
+
+        # Training
+        batch_size = 32
+        dropout_rate = 0.3
+        epochs = 500
+        patience = 10
+        min_delta = 0.001
+
+        unet_model = build_unet(dropout_rate=dropout_rate)
+        unet_model.compile(optimizer='adam',
+                           loss=dice_loss,
+                           metrics=['accuracy',
+                                    tf.keras.metrics.FalsePositives(thresholds=0.5, name='FP'),
+                                    tf.keras.metrics.FalseNegatives(thresholds=0.5, name='FN'),
+                                    tf.keras.metrics.TruePositives(thresholds=0.5, name='TP'),
+                                    tf.keras.metrics.TrueNegatives(thresholds=0.5, name='TN')])
+        history = unet_model.fit(X_train, y_train,
+                                 epochs=epochs)
+        unet_model.save(filename="Small_Unet.keras")
