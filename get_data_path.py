ce34f38e71921faed9648a2b94ca68fa2f2e4404
jrb187 <jefferson.bercaw@duke.edu>
2024-04-25 12:22:38 -0400

    Added task id control for data path

--- get_data_path.py
+++ get_data_path.py
@@ -3,9 +3,17 @@ import os
 
 def get_data_path():
     cur_dir = os.getcwd()
+    task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
+
     if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:":  # If on local machine
-        data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_BMP2"
+        if task_id == 5:
+            data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_BMP2"
+        elif task_id < 5:
+            data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Augmentations/sig" + str(task_id)
     else:  # If on cluster
-        data_path = "/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_BMP2"
+        if task_id == 5:
+            data_path = "/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_BMP2"
+        elif task_id < 5:
+            data_path = "/hpc/group/defratelab/jrb187/PatCartSeg/Augmentations/sig" + str(task_id)
 
     return data_path
