07ed1b622ed43dcc4d47cbb8c65911307578f768
jrb187 <jefferson.bercaw@duke.edu>
2024-04-29 17:03:58 -0400

    Added output metrics list

--- evaluate_physical_layer.py
+++ evaluate_physical_layer.py
@@ -5,9 +5,11 @@ from unet import build_unet
 from dice_loss_function import dice_loss
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from evaluate import plot_mri_with_both_masks
 
 
 if __name__ == "__main__":
+    # Histogram Figure
     with open('./augmentation/new_parameters.pkl', 'rb') as file:
         aug_dict = pickle.load(file)
         file.close()
@@ -27,6 +29,29 @@ if __name__ == "__main__":
     plt.xlabel("Blurring Factor $\sigma$")
     plt.legend(loc='best')
     plt.title("Motion artifact distributions")
+    plt.savefig("R:/DefratePrivate/Bercaw/Patella_Autoseg/results/motion_histogram.png", dpi=600)
     plt.show()
 
-    print("Done")
+    # Output Metrics
+    model_list = ["unet_2024-04-26_19-09-53_task0", "unet_2024-04-27_02-08-42_task1", "unet_2024-04-27_10-04-35_task2",
+                  "unet_2024-04-27_00-16-29_task3", "unet_2024-04-27_04-36-06_task4", "unet_2024-04-26_15-07-55_task5"]
+    motion_labels = ['Minimal', 'Mild', 'Moderate', 'Significant', 'Severe', 'No Motion']
+    table = list()
+    for idx, (model, label) in enumerate(zip(model_list, motion_labels)):
+        # Opening metrics file
+        with open(f'./results/{model}/metrics.pkl', 'rb') as file:
+            metrics = pickle.load(file)
+            file.close()
+
+        with open(f'./history/{model}.pkl', 'rb') as file:
+            hist = pickle.load(file)
+            file.close()
+
+        pd = metrics['patellar_dice']
+        pcd = metrics['patellar_cartilage_dice']
+        epochs = len(hist['FP'])
+
+        table.append([label, epochs, pd, pcd])
+
+    print(table)
+
