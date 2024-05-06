cda4b46a0f268f58c4d512393604f6681309e860
jrb187 <jefferson.bercaw@duke.edu>
2024-05-06 16:17:17 -0400

    Added dataset argument to get_data_path so desired dataset is selected upon running evaluate

--- get_data_path.py
+++ get_data_path.py
@@ -1,18 +1,10 @@
 import os
 
-
-def get_data_path():
+def get_data_path(dataset):
     cur_dir = os.getcwd()
 
     if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:":  # If on local machine
-        if task_id == 5:
-            data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_BMP2"
-        elif task_id < 5:
-            data_path = "R:/DefratePrivate/Bercaw/Patella_Autoseg/Augmentations/sig" + str(task_id)
+        data_path = f"R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_{dataset}"
     else:  # If on cluster
-        if task_id == 5:
-            data_path = "/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_BMP2"
-        elif task_id < 5:
-            data_path = "/hpc/group/ldefratelab/jrb187/PatCartSeg/Augmentations/sig" + str(task_id)
-
+        data_path = f"/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_{dataset}"
     return data_path
