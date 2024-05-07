8ab1ec1ca238c007a89e2ac00b9a5605161e8106
jrb187 <jefferson.bercaw@duke.edu>
2024-05-07 13:56:14 -0400

    Bug fix argparse parse_argsg

--- multiclass_segment.py
+++ multiclass_segment.py
@@ -14,7 +14,7 @@ from create_dataset import get_dataset
 
 parser = argparse.ArgumentParser(description="Training Options")
 parser.add_argument("-d", "--dataset", help="Dataset is either H or HT")
-args = parser.parse_args(["dataset"])
+args = parser.parse_args(["-d", "HT"])
 print(args.dataset)
 
 
