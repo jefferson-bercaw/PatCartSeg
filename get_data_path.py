2aa7d68b9ab203be3d27aa49632e5d407b6564bf
jrb187 <jefferson.bercaw@duke.edu>
2024-05-08 15:53:59 -0400

    Added "DefratePrivate" in cur_dir condition on line 7 to fix bug with raw server name

--- get_data_path.py
+++ get_data_path.py
@@ -4,7 +4,7 @@ import os
 def get_data_path(dataset):
     cur_dir = os.getcwd()
 
-    if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:":  # If on local machine
+    if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
         data_path = f"R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_{dataset}"
     else:  # If on cluster
         data_path = f"/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_{dataset}"
