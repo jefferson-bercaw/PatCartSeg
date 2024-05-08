bd9ec56487e0653316d84ca3974236eda3035cec
jrb187 <jefferson.bercaw@duke.edu>
2024-05-08 15:59:16 -0400

    Added os.sep to fix / vs \\ problem with filepaths

--- get_data_path.py
+++ get_data_path.py
@@ -5,7 +5,8 @@ def get_data_path(dataset):
     cur_dir = os.getcwd()
 
     if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
-        data_path = f"R:/DefratePrivate/Bercaw/Patella_Autoseg/Split_Data_{dataset}"
+        sep = os.sep
+        data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + f"Split_Data_{dataset}"
     else:  # If on cluster
         data_path = f"/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_{dataset}"
     return data_path
