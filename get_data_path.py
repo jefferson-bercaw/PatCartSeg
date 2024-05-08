5ea5048c3bf21a097f9397d4400e2b9de8210b5e
jrb187 <jefferson.bercaw@duke.edu>
2024-05-08 18:40:21 -0400

    Fixed bug on line 10 with missing separator before hpc

--- get_data_path.py
+++ get_data_path.py
@@ -7,5 +7,5 @@ def get_data_path(dataset):
     if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
         data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + f"Split_Data_{dataset}"
     else:  # If on cluster
-        data_path = "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + f"Split_Data_{dataset}"
+        data_path = sep + "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + f"Split_Data_{dataset}"
     return data_path
