517262adfc8803625b6324fb7d953dd9bcaf8571
jrb187 <jefferson.bercaw@duke.edu>
2024-05-08 16:12:02 -0400

    Added separator

--- get_data_path.py
+++ get_data_path.py
@@ -3,10 +3,9 @@ import os
 
 def get_data_path(dataset):
     cur_dir = os.getcwd()
-
+    sep = os.sep
     if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
-        sep = os.sep
         data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + f"Split_Data_{dataset}"
     else:  # If on cluster
-        data_path = f"/hpc/group/ldefratelab/jrb187/PatCartSeg/Split_Data_{dataset}"
+        data_path = "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + f"Split_Data_{dataset}"
     return data_path
