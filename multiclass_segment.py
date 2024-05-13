b8564aa807601fcfd0fe45d9b7e18c4285cf52b9
jrb187 <jefferson.bercaw@duke.edu>
2024-05-13 15:35:09 -0400

    Changed batch size to 24

--- multiclass_segment.py
+++ multiclass_segment.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     with strategy.scope():
         # Hyperparameters
-        batch_size = 12
+        batch_size = 24
         dropout_rate = 0.3
         epochs = 500
         patience = 20
