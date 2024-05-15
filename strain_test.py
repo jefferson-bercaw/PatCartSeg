cfd4cd789cef8fd854e41a15924021c885ab758e
jrb187 <jefferson.bercaw@duke.edu>
2024-05-15 16:19:02 -0400

    Fixed error of calculating nearest P point...implemented voxel lengths

--- strain_test.py
+++ strain_test.py
@@ -27,8 +27,8 @@ def return_predicted_volumes(subj_name, model_name):
     p_pred_names = [os.path.join(p_pred_folder, image_name) for image_name in image_list]
     pc_pred_names = [os.path.join(pc_pred_folder, image_name) for image_name in image_list]
 
-    p_vol = assemble_mri_volume(p_pred_names)
-    pc_vol = assemble_mri_volume(pc_pred_names)
+    p_vol = assemble_mri_volume(p_pred_names, xy_dim=256)
+    pc_vol = assemble_mri_volume(pc_pred_names, xy_dim=256)
 
     return p_vol, pc_vol
 
@@ -62,12 +62,17 @@ def get_outer_surface(pc_slice):
 
 
 def calculate_thickness(p_vol, pc_surf_mask):
+    voxel_lengths = [0.3, 0.3, 1.0]  # voxel lengths in mm
+
     p_thick_map = np.zeros_like(pc_surf_mask)
 
     pc_inds = np.argwhere(pc_surf_mask)
     p_inds = np.argwhere(p_vol)
 
-    distances = scipy.spatial.distance.cdist(pc_inds, p_inds)
+    pc_pos = pc_inds * voxel_lengths
+    p_pos = p_inds * voxel_lengths
+
+    distances = scipy.spatial.distance.cdist(pc_pos, p_pos)
     closest_indices = np.argmin(distances, axis=1)
 
     for i in range(len(pc_inds)):
@@ -120,18 +125,18 @@ def organize_coordinate_array(p_thick_map):
 def visualize_thickness_map(p_thick_map):
     coords_array = organize_coordinate_array(p_thick_map)
     point_cloud = pv.PolyData(np.transpose([coords_array[:, 0], coords_array[:, 1], coords_array[:, 2]]))
+    point_cloud['Cart. Thickness (mm)'] = coords_array[:, 3]
+
     surf = point_cloud.delaunay_2d()
     surf['Cart. Thickness (mm)'] = coords_array[:, 3]
 
     # Plot 3d triangle-ized surface
-    surf.plot(show_edges=False)
+    surf.plot(show_edges=True)
 
     # Plot point cloud
     # plotter = pv.Plotter()
     # plotter.add_points(point_cloud, point_size=20)
     # plotter.show()
-    thickness_values = coords_array[:, 2]
-    return thickness_values
 
 
 def plot_thickness_distributions(thickness_values, model_name):
@@ -149,8 +154,11 @@ def plot_thickness_distributions(thickness_values, model_name):
 if __name__ == '__main__':
 
     # Specify model name and subject name(s)
-    subj_names = ["AS_018", "AS_019", "AS_020", "AS_021", "AS_022", "AS_023"]
-    model_name = "unet_2024-04-17_08-06-28"
+    # subj_names = ["AS_018", "AS_019", "AS_020", "AS_021", "AS_022", "AS_023"]
+    # model_name = "unet_2024-04-17_08-06-28"
+
+    subj_names = ["AS_006", "AS_007", "AS_008", "AS_009", "AS_010", "AS_011"]
+    model_name = "unet_2024-05-15_07-23-17_cHT5"
 
     thickness_values = list()
 
@@ -163,15 +171,25 @@ if __name__ == '__main__':
 
         # Edit mask to get the right most pixels for the patellar cartilage
         pc_surf_mask = return_pc_surface(pc_vol)
+        num_cart_points = np.sum(pc_surf_mask)
+
+        # for slice_num in range(pc_surf_mask.shape[2]):
+        #     pc_surf_slice = pc_surf_mask[:, :, slice_num]
+        #     if np.max(pc_surf_slice) > 0:
+        #         plt.imshow(pc_surf_slice, cmap='gray')
+        #         plt.show()
+        #         plt.close()
 
         # For each cartilage surface point, calculate nearest patellar point, calculate distance, store in patella coord.
         p_thick_map = calculate_thickness(p_vol, pc_surf_mask)
 
         # Calculate coord array and store thickness values for this scan
         coords_array = organize_coordinate_array(p_thick_map)
+        num_cart_vox = len(coords_array)
+
         thickness_values.append(coords_array[:, 3])
 
         # Visualize the map
-        # visualize_thickness_map(p_thick_map)
+        visualize_thickness_map(p_thick_map)
 
     plot_thickness_distributions(thickness_values, model_name)
