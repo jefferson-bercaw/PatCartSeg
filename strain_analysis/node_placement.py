910ebfe58d5d683d62b47cf3801a6ea55825f585
jrb187 <jefferson.bercaw@duke.edu>
2024-05-28 16:12:30 -0400

    Strain assigned to pre cartilage

--- strain_analysis/node_placement.py
+++ strain_analysis/node_placement.py
@@ -19,27 +19,35 @@ if __name__ == "__main__":
     mean_points = np.mean(pre, axis=0)
     pre = pre - mean_points
     post = post - mean_points
-    strain_map[:, 0:3] = strain_map[:, 0:3] - mean_points
+    strain_coords = strain_map[:, 0:3]
+    strain_coords = strain_coords - mean_points
 
     # Perform PCA on pre, rotate pre and post. Determine if inferior-superior flipped or not.
     U, S, Vt = np.linalg.svd(pre)
     print(Vt.T)
     pre_rot = pre @ Vt.T
     post_rot = post @ Vt.T
+    strain_coords_rot = strain_coords @ Vt.T
 
+    # Create Point Clouds
     pre_rot_ptcld = o3d.geometry.PointCloud()
     pre_rot_ptcld.points = o3d.utility.Vector3dVector(pre_rot)
 
     post_rot_ptcld = o3d.geometry.PointCloud()
     post_rot_ptcld.points = o3d.utility.Vector3dVector(post_rot)
 
+    strain_ptcld = o3d.geometry.PointCloud()
+    strain_ptcld.points = o3d.utility.Vector3dVector(strain_coords_rot)
+
     # Visualize
     pre_rot_ptcld.paint_uniform_color([1, 0, 0])
     post_rot_ptcld.paint_uniform_color([0, 0, 1])
-    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-        size=40, origin=[0, 0, 0])
-    o3d.visualization.draw_geometries([pre_rot_ptcld, post_rot_ptcld, mesh_frame])
+    strain_ptcld.paint_uniform_color([0, 1, 0])
+
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=40, origin=[0, 0, 0])
+    o3d.visualization.draw_geometries([pre_rot_ptcld, post_rot_ptcld, strain_ptcld, mesh_frame])
 
     # Identify centroid of pre patella surface on articulating patella surface (extended in z dimension)
+
     # Move further along the z dimension to the strain map surface
     # Move along x and y dimensions on patella to place rest of nodes
