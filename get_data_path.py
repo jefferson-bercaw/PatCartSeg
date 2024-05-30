d31e9686d179040313b9a3c37ea93d40726b8a30
jrb187 <jefferson.bercaw@duke.edu>
2024-05-30 16:42:49 -0400

    Added "Paranjape" condition

--- get_data_path.py
+++ get_data_path.py
@@ -4,8 +4,15 @@ import os
 def get_data_path(dataset):
     cur_dir = os.getcwd()
     sep = os.sep
-    if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
-        data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + f"Split_Data_{dataset}"
-    else:  # If on cluster
-        data_path = sep + "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + f"Split_Data_{dataset}"
+    if "Paranjape" not in dataset:
+        if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
+            data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + f"Split_Data_{dataset}"
+        else:  # If on cluster
+            data_path = sep + "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + f"Split_Data_{dataset}"
+    elif "Paranjape" in dataset:
+        if cur_dir[0:2] == "R:" or cur_dir[0:2] == "C:" or "DefratePrivate" in cur_dir:  # If on local machine
+            data_path = "R:" + sep + "DefratePrivate" + sep + "Bercaw" + sep + "Patella_Autoseg" + sep + dataset
+        else:  # If on cluster
+            data_path = sep + "hpc" + sep + "group" + sep + "ldefratelab" + sep + "jrb187" + sep + "PatCartSeg" + sep + dataset
+
     return data_path
