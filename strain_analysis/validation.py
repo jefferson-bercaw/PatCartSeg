e35332fd89c544127fed8aa5b7c3cf86f05681b2
jrb187 <jefferson.bercaw@duke.edu>
2024-05-30 16:41:07 -0400

    Initial commit

---
+++ strain_analysis/validation.py
@@ -0,0 +1,9 @@
+import numpy as np
+from evaluate import load_model
+
+if __name__ == "__main__":
+    # Get filenames we're testing and figure out how many pre-post pairs we have
+    # Construct list of dictionaries of filenames of filename pairs, subjectID, Fr, duration in each
+    # Load in MRI volume
+
+    # Define and load in model using load_model
