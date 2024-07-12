54eec6a731509651508f03223e28cf5f0d4b11b3
jrb187 <jefferson.bercaw@duke.edu>
2024-07-12 16:08:50 -0400

    ctHT

--- multiclass_segment.py
+++ multiclass_segment.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         patience = 100
         min_delta = 0.0001
 
-        dataset = "ctHT5"
+        dataset = "ctHT"
 
         # Build and compile model
         unet_model = build_unet(model_depth=model_depth)
