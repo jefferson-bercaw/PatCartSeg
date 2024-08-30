f34e7cbaca722c44e9bb12b08050442d7cb7a63a
jrb187 <jefferson.bercaw@duke.edu>
2024-08-30 12:58:11 -0400

    Cropping 3D structures to 224x224x56

--- unet.py
+++ unet.py
@@ -9,12 +9,10 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 # Model Creation
 def double_conv_block(lyr, n_filt, kernel_size):
-    lyr = layers.Conv2D(n_filt, kernel_size, padding="same", kernel_initializer="he_normal")(lyr)
-    lyr = layers.BatchNormalization()(lyr)
+    lyr = layers.Conv3D(n_filt, kernel_size, padding="same", kernel_initializer="he_normal")(lyr)
     lyr = tf.keras.activations.relu(lyr)
 
-    lyr = layers.Conv2D(n_filt, kernel_size, padding="same", kernel_initializer="he_normal")(lyr)
-    lyr = layers.BatchNormalization()(lyr)
+    lyr = layers.Conv3D(n_filt, kernel_size, padding="same", kernel_initializer="he_normal")(lyr)
     lyr = tf.keras.activations.relu(lyr)
 
     return lyr
@@ -22,47 +20,45 @@ def double_conv_block(lyr, n_filt, kernel_size):
 
 def downsample_block(lyr, n_filt, kernel_size, dropout_rate):
     f = double_conv_block(lyr, n_filt, kernel_size)
-    p = layers.MaxPool2D(2)(f)
+    p = layers.MaxPool3D(2)(f)
     p = layers.Dropout(dropout_rate)(p)
-
     return f, p
 
 
+
 def upsample_block(lyr, conv_features, n_filt, kernel_size, dropout_rate):
-    lyr = layers.Conv2DTranspose(n_filt, kernel_size, 2, padding="same")(lyr)
-
+    lyr = layers.Conv3DTranspose(n_filt, kernel_size, 2, padding="same")(lyr)
     lyr = layers.concatenate([lyr, conv_features])
-
     lyr = layers.Dropout(dropout_rate)(lyr)
-
     lyr = double_conv_block(lyr, n_filt, kernel_size)
     return lyr
 
 
 def build_unet(model_depth):
-
     # inputs
-    inputs = layers.Input(shape=(256, 256, 1))
+    inputs = layers.Input(shape=(256, 256, 70, 1))  # Adjusted input shape
     dropout_rate = 0.3
 
     if model_depth == 3:
-
         start_filt = 256
 
         # encoder
         f1, p1 = downsample_block(inputs, n_filt=start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        print(f1.shape)
         f2, p2 = downsample_block(p1, n_filt=2 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
-
+        print(f2.shape)
         # bottleneck
         bottleneck = double_conv_block(p2, n_filt=4 * start_filt, kernel_size=3)
-
+        print(bottleneck.shape)
         # decoder
         u4 = upsample_block(bottleneck, conv_features=f2, n_filt=2 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        print(u4.shape)
         u5 = upsample_block(u4, conv_features=f1, n_filt=start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        outputs = layers.Conv2D(filters=2, kernel_size=1, padding="same", activation="sigmoid")(u5)
+        print(u5.shape)
+
+        outputs = layers.Conv2D(filters=2, kernel_size=1, padding="same", activation="sigmoid")(u5)  # Output shape: (256, 256, 70, 2)
 
     elif model_depth == 4:
-
         start_filt = 128
 
         # encoder
@@ -80,27 +76,25 @@ def build_unet(model_depth):
         outputs = layers.Conv2D(filters=2, kernel_size=1, padding="same", activation="sigmoid")(u7)
 
     elif model_depth == 5:
-
         start_filt = 64
 
         # encoder
         f1, p1 = downsample_block(inputs, n_filt=start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        f2, p2 = downsample_block(p1, n_filt=2*start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        f3, p3 = downsample_block(p2, n_filt=4*start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        f4, p4 = downsample_block(p3, n_filt=8*start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        f2, p2 = downsample_block(p1, n_filt=2 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        f3, p3 = downsample_block(p2, n_filt=4 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        f4, p4 = downsample_block(p3, n_filt=8 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
 
         # bottleneck
-        bottleneck = double_conv_block(p4, n_filt=16*start_filt, kernel_size=3)
+        bottleneck = double_conv_block(p4, n_filt=16 * start_filt, kernel_size=3)
 
         # decoder
-        u6 = upsample_block(bottleneck, conv_features=f4, n_filt=8*start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        u7 = upsample_block(u6, conv_features=f3, n_filt=4*start_filt, kernel_size=3, dropout_rate=dropout_rate)
-        u8 = upsample_block(u7, conv_features=f2, n_filt=2*start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        u6 = upsample_block(bottleneck, conv_features=f4, n_filt=8 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        u7 = upsample_block(u6, conv_features=f3, n_filt=4 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
+        u8 = upsample_block(u7, conv_features=f2, n_filt=2 * start_filt, kernel_size=3, dropout_rate=dropout_rate)
         u9 = upsample_block(u8, conv_features=f1, n_filt=start_filt, kernel_size=3, dropout_rate=dropout_rate)
         outputs = layers.Conv2D(filters=2, kernel_size=1, padding="same", activation="sigmoid")(u9)
 
     elif model_depth == 6:
-
         start_filt = 32
 
         # encoder
@@ -127,5 +121,5 @@ def build_unet(model_depth):
 
 
 if __name__ == "__main__":
-    unet_model = build_unet(model_depth=6)
+    unet_model = build_unet(model_depth=3)
     unet_model.summary()
