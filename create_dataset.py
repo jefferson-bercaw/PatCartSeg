67ced860a0ddf81df9f43b6746e2b2a38740ed08
jrb187 <jefferson.bercaw@duke.edu>
2024-09-03 14:59:53 -0400

    changed to all tensorflow ops

--- create_dataset.py
+++ create_dataset.py
@@ -15,7 +15,6 @@ def assemble_4d_mask(mask_3d):
 
     Outputs: mask_4d (tf.tensor) of size (xy_dim, xy_dim, depth, 2) of 1s and 0s corresponding to P and PC
     """
-
     p_mask_inds = tf.equal(mask_3d, 1)
     p = tf.where(p_mask_inds, 1, 0)
 
@@ -23,74 +22,27 @@ def assemble_4d_mask(mask_3d):
     pc = tf.where(pc_mask_inds, 1, 0)
 
     mask = tf.stack([p, pc], axis=-1)
-    mask = tf.squeeze(mask, axis=-3)
-    # tf.print(mask.shape)
+    mask = tf.squeeze(mask, axis=-2)
 
     return mask
 
 
-def load_train_scan(mri_folder, mask_folder):
+def load_images(mri_folder, mask_folder):
 
-    # train_path = get_data_path("CHT-Group")
-    # mri_path = os.path.join(train_path, "train", "mri", scan_folder)
-    # mask_path = os.path.join(train_path, "train", "mask", scan_folder)
+    mri_files = tf.io.matching_files(tf.strings.join([mri_folder, '/*.bmp']))
+    mask_files = tf.io.matching_files(tf.strings.join([mask_folder, '/*.bmp']))
 
-    # tf.print(mri_path)
+    mris = tf.map_fn(lambda img_file: tf.image.decode_bmp(tf.io.read_file(img_file)), mri_files, fn_output_signature=tf.uint8)
+    masks = tf.map_fn(lambda img_file: tf.image.decode_bmp(tf.io.read_file(img_file)), mask_files, fn_output_signature=tf.uint8)
 
-    mri_files = sorted(glob(os.path.join(mri_folder.numpy().decode("utf-8"), "*.bmp")))
-    mask_files = sorted(glob(os.path.join(mask_folder.numpy().decode("utf-8"), "*.bmp")))
-
-    # Load each image, resize if needed, and stack them
-    mris = [tf.image.decode_bmp(tf.io.read_file(img_file)) for img_file in mri_files]
-    masks = [tf.image.decode_bmp(tf.io.read_file(img_file)) for img_file in mask_files]
-
-    # Optionally resize images to (128, 224)
-    mris = [tf.image.resize(img, [224, 128]) for img in mris]
-    masks = [tf.image.resize(img, [224, 128]) for img in masks]
-
-    # Stack images to create a (224, 128, 56) tensor
-    mri_3d = tf.stack(mris, axis=-1)
-    mask_3d = tf.stack(masks, axis=-1)
-
-    mri_3d = tf.transpose(mri_3d, perm=[0, 1, 3, 2])
-    mask_4d = assemble_4d_mask(mask_3d)
-
-    mri_3d = tf.cast(mri_3d, tf.float64) / 255.0
+    mri_3d = tf.cast(mris, tf.float64) / 255.0
+    mask_4d = assemble_4d_mask(masks)
     mask_4d = tf.cast(mask_4d, tf.float64)
+
+    mri_3d = tf.transpose(mri_3d, perm=[1, 2, 0, 3])
+    mask_4d = tf.transpose(mask_4d, perm=[1, 2, 0, 3])
 
     return mri_3d, mask_4d
-
-
-def load_test_scan(mri_folder, mask_folder):
-
-    # train_path = get_data_path("CHT-Group")
-    # mri_path = os.path.join(train_path, "train", "mri", scan_folder)
-    # mask_path = os.path.join(train_path, "train", "mask", scan_folder)
-
-    # tf.print(mri_path)
-
-    mri_files = sorted(glob(os.path.join(mri_folder.numpy().decode("utf-8"), "*.bmp")))
-    mask_files = sorted(glob(os.path.join(mask_folder.numpy().decode("utf-8"), "*.bmp")))
-
-    # Load each image, resize if needed, and stack them
-    mris = [tf.image.decode_bmp(tf.io.read_file(img_file)) for img_file in mri_files]
-    masks = [tf.image.decode_bmp(tf.io.read_file(img_file)) for img_file in mask_files]
-
-    # Optionally resize images to (128, 224)
-    mris = [tf.image.resize(img, [224, 128]) for img in mris]
-    masks = [tf.image.resize(img, [224, 128]) for img in masks]
-
-    # Stack images to create a (224, 128, 56) tensor
-    mri_3d = tf.stack(mris, axis=-1)
-    mask_3d = tf.stack(masks, axis=-1)
-
-    mri_3d = tf.transpose(mri_3d, perm=[0, 1, 3, 2])
-    mask_4d = assemble_4d_mask(mask_3d)
-
-    mri_3d = tf.cast(mri_3d, tf.float64) / 255.0
-    mask_4d = tf.cast(mask_4d, tf.float64)
-
-    return mask_folder, mri_3d, mask_4d
 
 
 def visualize_dataset(dataset, num_samples=5):
@@ -141,18 +93,7 @@ def get_dataset(batch_size, dataset_type, dataset):
     mask_folders_ds = tf.data.Dataset.from_tensor_slices(mask_folders)
 
     dataset = tf.data.Dataset.zip((mri_folders_ds, mask_folders_ds))
-
-    # Map the load_train_scan function to each pair of folders
-    if dataset_type == 'train' or dataset_type == "val":
-        dataset = dataset.map(lambda mri_folder, mask_folder: tf.py_function(
-            func=load_train_scan,
-            inp=[mri_folder, mask_folder],
-            Tout=[tf.float64, tf.float64]))
-    elif dataset_type == "test":
-        dataset = dataset.map(lambda mri_folder, mask_folder: tf.py_function(
-            func=load_test_scan,
-            inp=[mri_folder, mask_folder],
-            Tout=[tf.string, tf.float64, tf.float64]))
+    dataset = dataset.map(load_images, num_parallel_calls=tf.data.AUTOTUNE)
 
     # randomly shuffle if training
     if dataset_type == "train":
