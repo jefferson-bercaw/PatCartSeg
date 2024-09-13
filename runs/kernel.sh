436427540e764e998fc95574b4712562bc9ea01c
jrb187 <jefferson.bercaw@duke.edu>
2024-09-13 15:37:19 -0400

    added batch size

--- runs/kernel.sh
+++ runs/kernel.sh
@@ -7,28 +7,28 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-allenlab-gpu-06
+#SBATCH --nodelist=dcc-allenlab-gpu-05
 
-python multiclass_segment.py --tissue=p --kernel=3
+python multiclass_segment.py --tissue=p --kernel=3 --batch_size=1
 python evaluate.py --tissue=p
 
-python multiclass_segment.py --tissue=p --kernel=5
+python multiclass_segment.py --tissue=p --kernel=5 --batch_size=1
 python evaluate.py --tissue=p
 
-python multiclass_segment.py --tissue=p --kernel=7
+python multiclass_segment.py --tissue=p --kernel=7 --batch_size=1
 python evaluate.py --tissue=p
 
-python multiclass_segment.py --tissue=p --kernel=9
+python multiclass_segment.py --tissue=p --kernel=9 --batch_size=1
 python evaluate.py --tissue=p
 
-python multiclass_segment.py --tissue=c --kernel=3
+python multiclass_segment.py --tissue=c --kernel=3 --batch_size=1
 python evaluate.py --tissue=c
 
-python multiclass_segment.py --tissue=c --kernel=5
+python multiclass_segment.py --tissue=c --kernel=5 --batch=1
 python evaluate.py --tissue=c
 
-python multiclass_segment.py --tissue=c --kernel=7
+python multiclass_segment.py --tissue=c --kernel=7 --batch=1
 python evaluate.py --tissue=c
 
-python multiclass_segment.py --tissue=c --kernel=9
+python multiclass_segment.py --tissue=c --kernel=9 --batch=1
 python evaluate.py --tissue=c
