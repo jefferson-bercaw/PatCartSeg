c5ca86967691883858a428f8098d535c3b43516a
jrb187 <jefferson.bercaw@duke.edu>
2024-09-16 11:26:18 -0400

    just last two

--- runs/kernel.sh
+++ runs/kernel.sh
@@ -9,15 +9,6 @@
 #SBATCH --exclusive
 #SBATCH --nodelist=dcc-allenlab-gpu-11
 
-python multiclass_segment.py --tissue=p --kernel=3 --batch=1
-python evaluate.py --tissue=p
-
-python multiclass_segment.py --tissue=p --kernel=5 --batch=1
-python evaluate.py --tissue=p
-
-python multiclass_segment.py --tissue=p --kernel=7 --batch=1
-python evaluate.py --tissue=p
-
 python multiclass_segment.py --tissue=p --kernel=9 --batch=1
 python evaluate.py --tissue=p
 
