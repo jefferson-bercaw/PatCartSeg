0b1872ed21ff19a01c59249075cbf8009884bbf8
jrb187 <jefferson.bercaw@duke.edu>
2024-09-16 10:02:11 -0400

    allenlab-gpu-11

--- runs/kernel.sh
+++ runs/kernel.sh
@@ -7,7 +7,7 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-allenlab-gpu-01
+#SBATCH --nodelist=dcc-allenlab-gpu-11
 
 python multiclass_segment.py --tissue=p --kernel=3 --batch=1
 python evaluate.py --tissue=p
