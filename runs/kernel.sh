deb1b7a0d306371e8217ba4d39ffaf67d5283652
jrb187 <jefferson.bercaw@duke.edu>
2024-09-16 11:27:13 -0400

    just last two

--- runs/kernel.sh
+++ runs/kernel.sh
@@ -7,7 +7,7 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-allenlab-gpu-11
+#SBATCH --nodelist=dcc-allenlab-gpu-05
 
 python multiclass_segment.py --tissue=p --kernel=9 --batch=1
 python evaluate.py --tissue=p
