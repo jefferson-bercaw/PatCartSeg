8bbc741deda399e67000ded26d15de1047f61300
jrb187 <jefferson.bercaw@duke.edu>
2024-09-19 10:35:26 -0400

    debugged model saving

--- 2d/multiclass_segment.py
+++ 2d/multiclass_segment.py
@@ -38,6 +38,8 @@ def save_model_info(model_info):
 
 
 if __name__ == "__main__":
+    # Main directory
+    main_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     # GPUs
     strategy = tf.distribute.MirroredStrategy()
@@ -85,11 +87,12 @@ if __name__ == "__main__":
         # Save model
         current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         model_name = f"unet2d-{parser.parse_args().tissue}_{current_time}_{dataset_name}"
-        unet_model.save(os.path.join("models", f"{model_name}.h5"))
+
+        unet_model.save(os.path.join(main_dir, "models", f"{model_name}.h5"))
 
         # Save history
         hist_name = f"{model_name}.pkl"
-        with open(os.path.join("history", hist_name), "wb") as f:
+        with open(os.path.join(main_dir, "history", hist_name), "wb") as f:
             pickle.dump(history.history, f)
 
         # Print saving model
