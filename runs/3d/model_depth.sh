ae7aa318db50c16a42f06314e845b888d8df8bf6
jrb187 <jrb187@duke.edu>
2024-10-03 13:00:57 -0400

    allenlab12

--- runs/3d/model_depth.sh
+++ runs/3d/model_depth.sh
@@ -7,12 +7,10 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-allenlab-gpu-08
+#SBATCH --nodelist=dcc-allenlab-gpu-03
 
 cd 3d
 
-python multiclass_segment.py --tissue=p --depth=3
-python evaluate.py --tissue=p
 
 python multiclass_segment.py --tissue=c --depth=3
 python evaluate.py --tissue=c
