aad0a1f4d9205618714c6be859cf32b4845f2434
jrb187 <jrb187@duke.edu>
2024-10-14 15:26:42 -0400

    fixed typo

--- runs/3d/bestc.sh
+++ runs/3d/bestc.sh
@@ -7,10 +7,10 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-biostat-gpu-01
+#SBATCH --nodelist=dcc-allenlab-gpu-03
 
 cd 3d
 
-python multiclass_segment.py --dataset=cHTCO-Group5Z --tissue=c --learningrate=0.00001 --batch=1 --depth=3 --dropout=0.1 -kernel=9 epochs=1000
+python multiclass_segment.py --dataset=cHTCO-Group5Z --tissue=c --learningrate=0.00001 --batch=1 --depth=3 --dropout=0.1 --kernel=9 --epochs=1000
 python evaluate.py --tissue=c
 
--- runs/3d/bestp.sh
+++ runs/3d/bestp.sh
@@ -7,10 +7,10 @@
 #SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --exclusive
-#SBATCH --nodelist=dcc-yaolab-gpu-08
+#SBATCH --nodelist=dcc-allenlab-gpu-01
 
 cd 3d
 
-python multiclass_segment.py --dataset=cHTCO-Group5 --tissue=p --learningrate=0.00001 --batch=1 --depth=4 --dropout=0.2 --kernel=5 epochs=1000
+python multiclass_segment.py --dataset=cHTCO-Group5 --tissue=p --learningrate=0.00001 --batch=1 --depth=4 --dropout=0.2 --kernel=5 --epochs=1000
 python evaluate.py --tissue=p
 
