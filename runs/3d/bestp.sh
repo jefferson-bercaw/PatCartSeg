9186d6309b74f0d2dbdde1917426604f0a37b885
jrb187 <jrb187@duke.edu>
2024-10-14 13:50:39 -0400

    training best networks

---
+++ runs/3d/bestp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --output=./outputs/%A_%a_slurm.out
+#SBATCH --error=./errors/%A_%a_slurm.err
+#SBATCH --nodes=1
+#SBATCH --mem=64G
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+#SBATCH --nodelist=dcc-yaolab-gpu-08
+
+cd 3d
+
+python multiclass_segment.py --dataset=cHTCO-Group5 --tissue=p --learningrate=0.00001 --batch=1 --depth=4 --dropout=0.2 --kernel=5 epochs=1000
+python evaluate.py --tissue=p
+
